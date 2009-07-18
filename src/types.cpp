2fc60abce12d55043788740784c10e981924e542
Vadim Alimguzhin <darth@darth.su>
2009-07-18 15:36:01 +0200

    Add type definitions and conversion functions.
      * add typedefs for GMP classes mpq_class and mpz_class
      * add conversion functions between GMP and FM rationals
      * add conversion function from timeval struct to milliseconds num

---
+++ src/types.cpp
@@ -0,0 +1,35 @@
+/** 
+ * @file types.cpp
+ * @brief Definition of functions from types.hpp.
+ * @author Vadim Alimguzhin <darth@darth.su>
+ * @date 18.07.2009
+ */
+
+#include "types.hpp"
+
+namespace mc_hybrid
+{
+  s_fm_rational_t*
+  real_t_to_fm_rational(real_t number)
+  {
+    s_fm_rational_t* result = fm_rational_alloc();
+    mpz_set(result->num, number.get_num_mpz_t());
+    mpz_set(result->denum, number.get_den_mpz_t());
+    return result;
+  }
+
+  real_t
+  fm_rational_to_real_t(s_fm_rational_t* number)
+  {
+    real_t result;
+    mpz_set(result.get_num_mpz_t(), number->num);
+    mpz_set(result.get_den_mpz_t(), number->denum);
+    return result;
+  }
+
+  ulong_t
+  timeval_to_ms(const timeval& t)
+  {
+    return t.tv_sec * 1000 + t.tv_usec / 1000;
+  }
+}; // namespace mc_hybrid
---
+++ src/types.hpp
@@ -0,0 +1,59 @@
+/** 
+ * @file types.hpp
+ * @brief Some typedefs and conversion functions declarations.
+ * @author Vadim Alimguzhin <darth@darth.su>
+ * @date 18.07.2009
+ */
+
+#ifndef TYPES_HPP_
+#define TYPES_HPP_
+
+#include <sys/time.h>
+
+#include <gmpxx.h>
+#include <fm/rational.h>
+#include <fm/system.h>
+#include <fm/solution.h>
+#include <fm/solver.h>
+
+/** 
+ * @brief Project namespace.
+ */
+namespace mc_hybrid
+{
+  typedef mpq_class real_t;      ///< Real type (GMP rational).
+  typedef mpz_class int_t;       ///< Integer type (GMP integer).
+  typedef unsigned long ulong_t; ///< Abbreviation for unsigned long.
+
+  /** 
+   * @brief Converts GMP rational into FM rational.
+   * 
+   * @param[in] number GMP rational.
+   * 
+   * @return FM rational.
+   */
+  s_fm_rational_t*
+  real_t_to_fm_rational(real_t number);
+
+  /** 
+   * @brief Converts FM rational into GMP rational.
+   * 
+   * @param[in] number FM rational.
+   * 
+   * @return GMP rational.
+   */
+  real_t
+  fm_rational_to_real_t(s_fm_rational_t* number);
+
+  /**
+   * @brief Converts timeval struct into ms (milliseconds).
+   *
+   * @param[in] t Timeval struct object.
+   *
+   * @return Number of ms equivalent to t.
+   */
+  ulong_t
+  timeval_to_ms(const timeval& t);
+}; // namespace mc_hybrid
+
+#endif // #ifndef TYPES_HPP_
