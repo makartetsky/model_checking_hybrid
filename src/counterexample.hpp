1fb972982feea73420b255e807b9a8692b0f67f8
Vadim Alimguzhin <darth@darth.su>
2009-07-19 14:20:13 +0200

    Add stub classes Counterexample and Model_smv.

---
+++ src/counterexample.hpp
@@ -0,0 +1,21 @@
+/** 
+ * @file counterexample.hpp
+ * @brief Counterexample class definition.
+ * @author Vadim Alimguzhin <darth@darth.su>
+ * @date 19.07.2009
+ */
+
+#ifndef COUNTEREXAMPLE_HPP_
+#define COUNTEREXAMPLE_HPP_
+
+namespace mc_hybrid
+{
+  /** 
+   * @brief Counterexample for verification process.
+   */
+  class Counterexample
+  {
+  }; // class Counterexample
+}; // namespace mc_hybrid
+
+#endif // #ifndef COUNTEREXAMPLE_HPP_
---
+++ src/model_smv.hpp
@@ -0,0 +1,21 @@
+/** 
+ * @file model_smv.hpp
+ * @brief Model_smv class definition.
+ * @author Vadim Alimguzhin <darth@darth.su>
+ * @date 19.07.2009
+ */
+
+#ifndef MODEL_SMV_HPP_
+#define MODEL_SMV_HPP_
+
+namespace mc_hybrid
+{
+  /** 
+   * @brief SMV model representation.
+   */
+  class Model_smv
+  {
+  }; // class Model_smv
+}; // namespace mc_hybrid
+
+#endif // #ifndef MODEL_SMV_HPP_
