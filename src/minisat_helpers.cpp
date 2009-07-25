a38278315357734336be25126f3a7f1946f66ea0
Vadim Alimguzhin <darth@darth.su>
2009-07-25 23:50:32 +0200

    Add Minisat+ helper functions.

---
+++ src/minisat_helpers.cpp
@@ -0,0 +1,151 @@
+/**
+ * @file minisat_helpers.cpp
+ * @brief Helper functions for minisat+ using definition.
+ * @author Vadim Alimguzhin <darth@darth.su>
+ * @date 22.07.2009
+ */
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <stdexcept>
+
+#include "types.hpp"
+#include "variable.hpp"
+#include "constraint.hpp"
+#include "problem.hpp"
+#include "minisat_helpers.hpp"
+
+using std::string;
+using std::ostringstream;
+using std::fstream;
+using std::endl;
+using std::ios;
+using std::remove;
+using std::runtime_error;
+using std::vector;
+using std::istringstream;
+
+namespace mc_hybrid
+{
+  const char* minisat_input_path = "./minisat_input";
+  const char* minisat_output_path = "./minisat_output";
+  const char* minisat_exec_path = "./minisat+.exe";
+  
+  size_t
+  minisat_launch(Problem* problem,
+          Problem::Constrs_group group,
+          size_t aux_num,
+          string& cnf)
+  {
+    minisat_generate_input(problem, group);
+
+    string minisat_cmd(minisat_exec_path);
+    minisat_cmd += " ";
+    minisat_cmd += minisat_input_path;
+    minisat_cmd += " -cnf=";
+    minisat_cmd += minisat_output_path;
+    minisat_cmd += " > /dev/null";
+    int result = system(minisat_cmd.c_str());
+    if (result != 0)
+      throw runtime_error("Minisat+ can't be launched");
+    remove(minisat_input_path);
+
+    size_t new_aux_num = minisat_read_output(aux_num, cnf);
+
+    remove(minisat_output_path);
+
+    return new_aux_num;
+  }
+
+  void
+  minisat_generate_input(Problem* problem,
+                         Problem::Constrs_group group)
+  {
+    fstream file(minisat_input_path, ios::out | ios::trunc);
+    if (!file)
+      throw runtime_error("Can't create minisat+ input file.");
+    
+    for (size_t i = 0; i < problem->get_constraints_num(group); ++i)
+    {
+      Constraint& c = problem->get_constraint(group, i);
+      for (size_t j = 0; j < problem->get_constraints_vars_num(group); ++j)
+      {
+        Variable& v = problem->get_constraints_var(group, j);
+        real_t coeff = c.get_coeff(v);
+        if (coeff != 0)
+          file << coeff << "*" << v.get_name() << " ";
+      }
+      file << ">= " << -1 * c.get_free_member() << ";" << endl;
+    }
+  }
+
+  size_t
+  minisat_read_output(size_t aux_num,
+                      string& cnf)
+  {
+    ostringstream oss_result;
+    fstream file(minisat_output_path, ios::in);
+    if (!file)
+      throw runtime_error("Minisat+ output file doesn't exist.");
+
+    // read names
+    vector<string> index2name;
+    string names;
+    getline(file, names);
+    istringstream iss(names);
+    string name;
+    while (iss >> name)
+    {
+      // replace ' with next()
+      if (name[name.length() - 1] == '\'')
+      {
+        name.erase(name.length() - 1);
+        name = "next(" + name + ")";
+      }
+      index2name.push_back(name);
+    }
+    size_t orig_number = index2name.size();
+
+    // Read vars and clauses num.
+    size_t vars_num;
+    file >> vars_num;
+    size_t clauses_num;
+    file >> clauses_num;
+    file.ignore();
+
+    // Add enough num of aux vars to index2name.
+    size_t aux_index = aux_num;
+    for (size_t i = orig_number; i < vars_num; ++i)
+    {
+      ostringstream oss;
+      oss << "_aux" << aux_index;
+      index2name.push_back(oss.str());
+      ++aux_index;
+    }
+
+    // Read clauses.
+    string clause;
+    while (getline(file, clause))
+    {
+      istringstream iss(clause);
+      oss_result << "(";
+      int var_index;
+      while (iss >> var_index)
+      {
+        if (var_index < 0)
+        {
+          oss_result << "!";
+          var_index *= -1;
+        }
+        oss_result << index2name.at(var_index);
+        oss_result << " | ";
+      }
+      oss_result.seekp(-3, ios::end);
+      oss_result << ") & ";
+    }
+    cnf = oss_result.str();
+    cnf.erase(cnf.length() - 3);
+    return aux_index;
+  }
+}; // namespace mc_hybrid
---
+++ src/minisat_helpers.hpp
@@ -0,0 +1,58 @@
+/**
+ * @file minisat_helpers.hpp
+ * @brief Helper functions for using minisat+ declaration.
+ * @author Vadim Alimguzhin <darth@darth.su>
+ * @date 22.07.2009
+ */
+
+#ifndef MINISAT_HELPERS_HPP_
+#define MINISAT_HELPERS_HPP_
+
+#include <string>
+
+namespace mc_hybrid
+{
+  extern const char* minisat_input_path;  ///< Path to minisat+ input file.
+  extern const char* minisat_output_path; ///< Path to minisat+ output file.
+  extern const char* minisat_exec_path;   ///< Path to minisat+ executable.
+
+  /**
+   * @brief Makes cnf equisatisfiable to system of pb constraints.
+   * 
+   * @param[in]  problem Problem.
+   * @param[in]  group   Constraints group.
+   * @param[in]  aux_num Aux vars number.
+   * @param[out] cnf     Result string with CNF.
+   *
+   * @return New aux vars number.
+   */
+  size_t
+  minisat_launch(Problem* problem,
+          Problem::Constrs_group group,
+          size_t aux_num,
+          std::string& cnf);
+
+  /**
+   * @brief Makes input file for minisat+.
+   * 
+   * @param[in]  problem Problem.
+   * @param[in]  group   Constraints group.
+   */
+  void
+  minisat_generate_input(Problem* problem,
+                         Problem::Constrs_group group);
+
+  /**
+   * @brief Reads minisat output file.
+   *
+   * @param[in]  aux_num Aux vars number.
+   * @param[out] cnf     Result string with CNF.
+   *
+   * @return New aux vars number.
+   */
+  size_t
+  minisat_read_output(size_t aux_num,
+                      std::string& cnf);
+}; // namespace mc_hybrid
+
+#endif // #ifndef MINISAT_HELPERS_HPP_
