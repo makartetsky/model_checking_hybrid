bfc9a41ff8ce8d846ec53c91a20a62ba5f3811fa
Vadim Alimguzhin <darth@darth.su>
2009-07-25 23:53:56 +0200

    Implement Model_smv class (without refinement).

---
+++ src/model_smv.cpp
@@ -0,0 +1,121 @@
+/**
+ * @file model_smv.cpp
+ * @brief Model_smv class methods definition.
+ * @author Vadim Alimguzhin <darth@darth.su>
+ * @date 25.07.2009
+ */
+
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+
+#include "types.hpp"
+#include "model_smv.hpp"
+#include "counterexample.hpp"
+#include "variable.hpp"
+#include "constraint.hpp"
+#include "problem.hpp"
+#include "minisat_helpers.hpp"
+
+using std::string;
+using std::fstream;
+using std::ios;
+using std::runtime_error;
+using std::endl;
+using std::ostringstream;
+
+namespace mc_hybrid
+{
+  const char* nusmv_input_path = "./nusmv_input";
+  const char* nusmv_output_path = "./nusmv_output";
+  const char* nusmv_exec_path = "NuSMV";
+
+  Model_smv::Model_smv(Problem* problem)
+  {
+    // fill variables names from problem
+    for (size_t i = 0; i < problem->get_variables_num(Problem::VARS_INPUT); ++i)
+    {
+      Variable& v = problem->get_variable(Problem::VARS_INPUT, i);
+      vars_input.push_back(v.get_name());
+    }
+    for (size_t i = 0; i < problem->get_variables_num(Problem::VARS_STATE); ++i)
+    {
+      Variable& v = problem->get_variable(Problem::VARS_STATE, i);
+      vars_state.push_back(v.get_name());
+    }
+    size_t aux_num = 0;
+    aux_num = minisat_launch(problem,
+                             Problem::CONSTRS_INIT,
+                             aux_num,
+                             init);
+    aux_num = minisat_launch(problem,
+                             Problem::CONSTRS_TRANS,
+                             aux_num,
+                             trans);
+    aux_num = minisat_launch(problem,
+                             Problem::CONSTRS_SPEC,
+                             aux_num,
+                             spec);
+    for (size_t i = 0; i < aux_num; ++i)
+    {
+      ostringstream oss;
+      oss << "_aux" << i;
+      vars_state.push_back(oss.str());
+    }
+  }
+
+  bool
+  Model_smv::refine(Counterexample* /*counterexample*/)
+  {
+    return false;
+  }
+
+  void
+  Model_smv::write(const string& filename)
+  {
+    fstream file(filename.c_str(), ios::out | ios::trunc);
+    if (!file)
+      throw runtime_error("Can't create NuSMV input file.");
+
+    file << "MODULE main" << endl;
+    file << "IVAR" << endl;
+    for (size_t i = 0; i < vars_input.size(); ++i)
+      file << "  " << vars_input.at(i) << " : 0 .. 1;" << endl;
+
+    file << "VAR" << endl;
+    for (size_t i = 0; i < vars_state.size(); ++i)
+      file << "  " << vars_state.at(i) << " : 0 .. 1;" << endl;
+        
+    file << "INIT" << endl;
+    file << "  " << init << ";" << endl;
+
+    file << "TRANS" << endl;
+    file << "  " << trans  << ";" << endl;
+    
+    file << "SPEC" << endl;
+    file << "  AG (" << spec << ");" << endl;
+  }
+
+  bool
+  Model_smv::verify()
+  {
+    write(nusmv_input_path);
+
+    string nusmv_cmd = nusmv_exec_path;
+    nusmv_cmd += " ";
+    nusmv_cmd += nusmv_input_path;
+    nusmv_cmd += " > ";
+    nusmv_cmd += nusmv_output_path;
+
+    if (system(nusmv_cmd.c_str()) != 0)
+      throw runtime_error("NuSMV can't be launched.");
+
+    remove(nusmv_input_path);
+
+    // Read nusmv output.
+    
+    remove(nusmv_output_path);
+
+    return false;
+  }
+}; // namespace mc_hybrid
--- src/model_smv.hpp
+++ src/model_smv.hpp
@@ -8,13 +8,68 @@
 #ifndef MODEL_SMV_HPP_
 #define MODEL_SMV_HPP_
 
+#include <vector>
+#include <string>
+
 namespace mc_hybrid
 {
+  class Problem;
+  class Counterexample;
+
+  extern const char* nusmv_input_path;
+  extern const char* nusmv_output_path;
+  extern const char* nusmv_exec_path;
+
   /**
    * @brief SMV model representation.
    */
   class Model_smv
   {
+    public:
+      /**
+       * @brief Constructor from Problem.
+       *
+       * @attention Problem must be pseudoboolean.
+       *
+       * @param[in] problem Problem.
+       */
+      Model_smv(Problem* problem);
+
+      /**
+       * @brief Refines model
+       * 
+       * @return true if refinement succeeds, false otherwise.
+       */
+      bool
+      refine(Counterexample* c);
+
+      /**
+       * @brief Writes model to file with specified name.
+       * 
+       * @param[in] filename Filename.
+       */
+      void
+      write(const std::string& filename);
+
+      /**
+       * @brief Verify model using NuSMV model checker.
+       * 
+       * @return true if model fits specification, false otherwise.
+       */
+      bool
+      verify();
+
+    public:
+      std::vector<std::string> vars_input; ///< Input variables.
+      std::vector<std::string> vars_state; ///< State variables.
+
+      std::string init;  ///< Initial conditions.
+      std::string trans; ///< Transitional relation.
+      std::string spec;  ///< Specification.
+
+    private:
+      size_t tmax;                      ///< Maximal length of counterexample.
+      std::vector<std::string> defines; ///< Defines.
   }; // class Model_smv
 }; // namespace mc_hybrid
 
